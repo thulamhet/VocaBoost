140f58834acc1de6354b98bfa5c251db0aa3da9d
Nguyen Cong Thu <thunc@vnpay.vn>
2025-03-31 11:25:41 +0700

    feat: add keychain service

---
+++ VocaBoost/Supports/Helper/KeychainService.swift
@@ -0,0 +1,56 @@
+//
+//  KeychainService.swift
+//  VocaBoost
+//
+//  Created by Nguyễn Công Thư on 28/3/25.
+//
+
+import Foundation
+import Security
+
+struct KeychainService {
+    static let service = "com.VocaBoost.supabase"
+    static let account = "supabaseSession"
+
+    static func saveSession(_ session: String) {
+        let data = session.data(using: .utf8)!
+
+        deleteSession()
+
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account,
+            kSecValueData as String: data
+        ]
+
+        SecItemAdd(query as CFDictionary, nil)
+    }
+
+    static func loadSession() -> String? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account,
+            kSecReturnData as String: kCFBooleanTrue!,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+
+        var dataTypeRef: AnyObject?
+        let status = SecItemCopyMatching(query as CFDictionary, &dataTypeRef)
+
+        if status == errSecSuccess, let data = dataTypeRef as? Data {
+            return String(data: data, encoding: .utf8)
+        }
+        return nil
+    }
+
+    static func deleteSession() {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account
+        ]
+        SecItemDelete(query as CFDictionary)
+    }
+}
