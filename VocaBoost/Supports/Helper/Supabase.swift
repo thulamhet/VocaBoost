b83b3e4532d427e50e2ae40215742c4f99e1ee42
Nguyen Cong Thu <thunc@vnpay.vn>
2025-03-31 11:25:53 +0700

    ref: supabase manager

--- VocaBoost/Supports/Helper/Supabase.swift
+++ VocaBoost/Supports/Helper/Supabase.swift
@@ -8,7 +8,13 @@
 import Foundation
 import Supabase
 
-let supabase = SupabaseClient(
-    supabaseURL: URL(string: "https://mqgbouveuxemqhvpbdeu.supabase.co")!,
-    supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xZ2JvdXZldXhlbXFodnBiZGV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA2NDExOTQsImV4cCI6MjA1NjIxNzE5NH0.L4jvjafIK8UdCHVyW-QcDXGCbPLelT5wtrSiscjAZcE"
-)
+class SupabaseManager {
+    static let shared = SupabaseClient(
+        supabaseURL: URL(string: "https://mqgbouveuxemqhvpbdeu.supabase.co")!,
+        supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xZ2JvdXZldXhlbXFodnBiZGV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA2NDExOTQsImV4cCI6MjA1NjIxNzE5NH0.L4jvjafIK8UdCHVyW-QcDXGCbPLelT5wtrSiscjAZcE"
+    )
+
+    private init () {}
+}
+
+let supabase = SupabaseManager.shared
